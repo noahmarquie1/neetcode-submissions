ca3cda551e820faedee136decb538fb9fad467ce
Noah Marquie <87723564+noahmarquie1@users.noreply.github.com>
2026-06-28 09:09:43 -0700

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # '\n' is not a valid ascii character in python (seen as special function)
+        # '\s' at start (if nonempty) and '\e' at end (if nonempty)
+        encoded_str = ""
+        for i, string in enumerate(strs):
+            if i == 0:
+                encoded_str += '\s'
+
+            encoded_str += string
+            if i < len(strs) - 1:
+                encoded_str += '\n'
+            else:
+                encoded_str += '\e'
+
+        return encoded_str
+
+
+    def decode(self, s: str) -> List[str]:
+        if '\s' in s and '\e' in s:
+            str_content = s.split('\s')[1].split('\e')[0]
+            return str_content.split('\n')
+
+        return []
